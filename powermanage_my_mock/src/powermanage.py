37d6b06d6cfacf35a53902125abde3326c90529b
Alejandro Lorente <alejandro.lorente@qvantel.com>
2019-02-27 19:55:20 +0100

    add the method

--- powermanage_my_mock/src/powermanage.py
+++ powermanage_my_mock/src/powermanage.py
@@ -2,11 +2,11 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route("/scripts/update.php")
+@app.route("/scripts/update.php", methods=['GET'])
 def keep_alive():
     return Response("status=0&ka_time=50&allow=0", mimetype='text/plain')
 
 
-@app.route("/")
-def test():
+@app.route("/scripts/notify.php", methods=['POST'])
+def event_notified():
     return "hola mundo"
