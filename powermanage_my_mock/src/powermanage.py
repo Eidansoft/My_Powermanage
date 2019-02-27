6289def0f0376da1a3ce00cc49a9b31164601a46
Alejandro Lorente <alejandro.lorente@qvantel.com>
2019-02-27 19:55:42 +0100

    add the library to parse easely the xml sent by the powermaster

--- powermanage_my_mock/src/powermanage.py
+++ powermanage_my_mock/src/powermanage.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response
+import xmltodict
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def keep_alive():
 
 @app.route("/scripts/notify.php", methods=['POST'])
 def event_notified():
+    xmltodict.parse(request.data)['xml']['From']
     return "hola mundo"
