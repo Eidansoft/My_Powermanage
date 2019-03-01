d5622f383750b057a5bbdc580c29b07d8add0910
Alejandro Lorente <alejandro.lorente@qvantel.com>
2019-03-01 21:55:37 +0100

    add a logger to be able to investigate the call from the powermaster

--- powermanage_my_mock/src/powermanage.py
+++ powermanage_my_mock/src/powermanage.py
@@ -4,14 +4,16 @@ import xmltodict
 @route("/scripts/update.php")
 def keep_alive():
     response.content_type = 'text/plain; charset=utf-8'
+    log(request)
     return "status=0&ka_time=50&allow=0"
 
 
 @route("/scripts/notify.php", method='POST')
 def event_notified():
-    raw_body = request.body.read()
+    raw_body = request.body.read().decode('utf-8')
     index_value_sent = xmltodict.parse(raw_body)['notify']['index']
     response.content_type = 'text/xml;charset=utf-8'
+    log(request)
     return "<?xml version '1.0'?> <index>{}</index>".format(index_value_sent)
 
 
@@ -19,5 +21,19 @@ def event_notified():
 def hello():
     return "Hello World!"
 
+
+def log(request):
+    result = {
+        'method': request.method,
+        'headers': dict(request.headers),
+        'body': request.body.read().decode('utf-8'),
+        'files': [
+            {'key': key, 'name': request.files[key].raw_filename}
+            for key in request.files
+        ]
+    }
+    print('#################################')
+    print(result)
+    print('#################################')
 
 run(host='0.0.0.0', port=8080, debug=True)
